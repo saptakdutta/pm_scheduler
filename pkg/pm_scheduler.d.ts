3a2b61fcdca91a65ccf04c715938faf7c6bbcea5
Saptak Dutta <saptakdutta@cmail.carleton.ca>
2026-09-02 12:54:23 -0400

    Add files via upload

---
+++ pkg/pm_scheduler.d.ts
@@ -0,0 +1,47 @@
+/* tslint:disable */
+/* eslint-disable */
+
+export function load_projects(file_contents: string): string;
+
+export function make_project(name: string, hours: number, colleagues_csv: string, milestones_json: string): string;
+
+export function serialize_projects(projects_json: string): string;
+
+export function total_hours(projects_json: string): number;
+
+export type InitInput = RequestInfo | URL | Response | BufferSource | WebAssembly.Module;
+
+export interface InitOutput {
+    readonly memory: WebAssembly.Memory;
+    readonly load_projects: (a: number, b: number) => [number, number];
+    readonly make_project: (a: number, b: number, c: number, d: number, e: number, f: number, g: number) => [number, number];
+    readonly serialize_projects: (a: number, b: number) => [number, number];
+    readonly total_hours: (a: number, b: number) => number;
+    readonly __wbindgen_externrefs: WebAssembly.Table;
+    readonly __wbindgen_malloc: (a: number, b: number) => number;
+    readonly __wbindgen_realloc: (a: number, b: number, c: number, d: number) => number;
+    readonly __wbindgen_free: (a: number, b: number, c: number) => void;
+    readonly __wbindgen_start: () => void;
+}
+
+export type SyncInitInput = BufferSource | WebAssembly.Module;
+
+/**
+ * Instantiates the given `module`, which can either be bytes or
+ * a precompiled `WebAssembly.Module`.
+ *
+ * @param {{ module: SyncInitInput }} module - Passing `SyncInitInput` directly is deprecated.
+ *
+ * @returns {InitOutput}
+ */
+export function initSync(module: { module: SyncInitInput } | SyncInitInput): InitOutput;
+
+/**
+ * If `module_or_path` is {RequestInfo} or {URL}, makes a request and
+ * for everything else, calls `WebAssembly.instantiate` directly.
+ *
+ * @param {{ module_or_path: InitInput | Promise<InitInput> }} module_or_path - Passing `InitInput` directly is deprecated.
+ *
+ * @returns {Promise<InitOutput>}
+ */
+export default function __wbg_init (module_or_path?: { module_or_path: InitInput | Promise<InitInput> } | InitInput | Promise<InitInput>): Promise<InitOutput>;
---
+++ pkg/pm_scheduler_bg.wasm.d.ts
@@ -0,0 +1,12 @@
+/* tslint:disable */
+/* eslint-disable */
+export const memory: WebAssembly.Memory;
+export const load_projects: (a: number, b: number) => [number, number];
+export const make_project: (a: number, b: number, c: number, d: number, e: number, f: number, g: number) => [number, number];
+export const serialize_projects: (a: number, b: number) => [number, number];
+export const total_hours: (a: number, b: number) => number;
+export const __wbindgen_externrefs: WebAssembly.Table;
+export const __wbindgen_malloc: (a: number, b: number) => number;
+export const __wbindgen_realloc: (a: number, b: number, c: number, d: number) => number;
+export const __wbindgen_free: (a: number, b: number, c: number) => void;
+export const __wbindgen_start: () => void;
